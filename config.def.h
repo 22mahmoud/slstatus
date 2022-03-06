a3611cba43882958c4c5c5dce2d74785d8dc7b8b
22mahmoud <hello@mahmoudashraf.dev>
2022-03-06 11:21:16 +0200

    use pamisxer instead

--- config.def.h
+++ config.def.h
@@ -17,7 +17,7 @@ static const struct arg args[] = {
 
 	{ separator,	 " | ",              NULL           },
 
-	{ run_command, ": %3s", "amixer sget Master | awk -F'[][]' '/Left:/ { print $2 }'" },
+	{ run_command, ": %2s", "pamixer --get-volume" },
 
 	{ separator,	 " | ",              NULL           },
 	{ cpu_perc, "[CPU %s%%]", NULL	      },
